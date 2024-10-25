8c5b956c97b1f7bec29aa638047ba145da0982a3
kyrenieee <kcestocapio05@gmail.com>
2024-10-25 16:03:54 +0800

    practice fetch & async

--- java.js
+++ java.js
@@ -1,9 +1,22 @@
-fetch('https://api.harvardartmuseums.org')
-    .then(res => {
-        if(res.ok){
-            console
-        }
-        res.json()
-    })
-    .then(data => console.log(data))
+//fetch("https://theaxolotlapi.netlify.app/")
+//    .then(response => {
+//
+//        if(response.ok){
+//            throw new Error("Could not fetch resource.");
+//        }
+//        return response.json();
+//    })
+//    .then(data => console.log(data))
+//    .catch(error => console.error(error)); 
 
+async function FetchData() {
+
+    try{
+        const response = await fetch();
+    }
+
+    catch(error){
+        console.error(error);
+    }
+    
+}
