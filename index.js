bd51b5d44f861ca26f82b535334ded3542aaa152
kyrenieee <kcestocapio05@gmail.com>
2024-10-28 14:35:07 +0800

    added background and fixed api

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 console.log("JavaScript loaded successfully!");
 
-FetchData();
-async function FetchData() {
+async function fetchData() {
+  const pokemon = document.getElementById("Pokemons").value.toLowerCase();
 
-    try{
-        const Pokemon = document.getElementById("Pokemons").value.toLowerCase;
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${Pokemon}`);
-        
-
-        if(!response.ok){
-            throw new Error("Could not fetch resource");
-        }
-
-        const data = await response.json();
-        const PokemonSprite = data.sprites.front_default;
-        const ImgElement = document.getElementById("PokemonSprite");
-
-        ImgElement.src = PokemonSprite;
-        ImgElement.style.display = "block";
+    if (!response.ok) {
+      throw new Error("Could not fetch resource");
     }
 
-    catch(error){
-        console.error(error);
+    const data = await response.json();
+    const pokemonSprite = data.sprites.front_default;
+    const imgElement = document.getElementById("PokemonSprite");
+
+    imgElement.src = pokemonSprite;
+    imgElement.style.display = "block";
+  } 
+    catch (error) {
+    console.error(error);
     }
-    
 }
