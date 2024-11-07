474a60746c840a075c74527870e8a79385e88117
kyrenieee <>
2024-11-07 08:06:28 +0800

    testing data fetching

--- index.js
+++ index.js
@@ -2,20 +2,29 @@ console.log("JavaScript loaded successfully!");
 
 async function fetchData() {
   const pokemon = document.getElementById("Pokemons").value.toLowerCase();
-
+  console.log(pokemonSprite)
   try {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+// website/type/pokemon/atrib/moreatrib
+    const jkl = pokemon ? pokemon : "pikachu";
+    //condition ? exprIfTrue : exprIfFalse0
+    console.log("sdfdfdf", jkl);
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${jkl} `);
+    
 
     if (!response.ok) {
       throw new Error("Could not fetch resource");
     }
-
+  
     const data = await response.json();
     const pokemonSprite = data.sprites.front_default;
+    const pokemonData = data.stats;
     const imgElement = document.getElementById("PokemonSprite");
 
     imgElement.src = pokemonSprite;
     imgElement.style.display = "block";
+
+    document.body.getElementById("PokemonStat")=pokemonData;
+
   } 
     catch (error) {
     console.error(error);
